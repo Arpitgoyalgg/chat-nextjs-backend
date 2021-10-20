657fe6f89e32502cb8a8448044146ecd89c085d4
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2021-10-20 17:25:37 +0530

    add a sample users map and generate a list of users based on it to send users list

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+let user_room_map = {'Arpit': '', 'Vaibhav': '', 'Hari': '', 'Shyam': ''};
+let users = []
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  users = []
+  Object.keys(user_room_map).map(function (key,index) {
+    users.push(key)
+  })
+  console.log(`sending - ${users}`)
+  res.send(users);
 });
 
 module.exports = router;
