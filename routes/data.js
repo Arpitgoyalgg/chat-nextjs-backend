4cf968c2589a0521b2fe990da8b3387e330cd8ef
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2021-10-20 15:41:33 +0530

    add endpoint data

---
+++ routes/data.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET chat data. */
+router.get('/', function(req, res, next) {
+    res.send('data endpoint');
+});
+
+/* GET chat data. */
+router.get('/all-chats', function(req, res, next) {
+    res.send('all chats for a user');
+});
+
+module.exports = router;
