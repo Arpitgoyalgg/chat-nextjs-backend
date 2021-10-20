0779bdc94581ccaee80d2f50daff30a4a7b14046
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2021-10-20 18:01:52 +0530

    add user functionality

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-let user_room_map = {'Arpit': '', 'Vaibhav': '', 'Hari': '', 'Shyam': ''};
+// let user_room_map = {'Arpit': '', 'Vaibhav': '', 'Hari': '', 'Shyam': ''};
 let users = []
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  users = []
-  Object.keys(user_room_map).map(function (key,index) {
-    users.push(key)
-  })
+  // users = []
+  // Object.keys(user_room_map).map(function (key,index) {
+  //   users.push(key)
+  // })
   console.log(`sending - ${users}`)
   res.send(users);
+});
+
+/* POST add user. */
+router.post('/add', function(req, res, next) {
+  console.log(req.body.name)
+  users.push(req.body.name)
+  res.send('add user');
 });
 
 module.exports = router;
