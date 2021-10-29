c64cf81789ae7fbcbcee416cdee69ebe0497da79
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2021-10-29 13:43:53 +0530

    remove postgres support

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db')
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-
-  // fetch users from postgres database heroku
-  const client = await db.connect();
-  const result = await client.query('SELECT * FROM Users');
-  console.log(result.rows)
-  res.send(result.rows);
+router.get('/', function(req, res, next) {
+  res.send('users list');
 });
 
 /* POST add user. */
-router.post('/add', async function(req, res, next) {
-  const user = req.body.name
-  console.log(`data coming - ${user}`)
-
-  // add user to postgres database heroku
-  const client = await db.connect();
-  const result = await client.query(`INSERT INTO Users(name) VALUES('${user}')`);
-
+router.post('/add', function(req, res, next) {
   res.send('add user');
 });
 
